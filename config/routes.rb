32993423bab55b2e7cf96bef6507a00831fc1ae8
alex.grigorenko2942@gmail.com <alex.grigorenko2942@gmail.com>
2019-08-18 18:45:28 +0300

    added pdf export of diplomas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'exports/download'
   get 'top_screen/search'
   post 'top_screen/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
